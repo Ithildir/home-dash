2a3f66f5c7b0cf325c9d1d6200b8a250bf1493e9
Andrea Di Giorgi <adigiorgi@outlook.com>
2020-01-14 19:42:44 -0800

    Send weather info to InfluxDB

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM balenalib/raspberrypi3-python:3-build
 
-RUN install_packages python3-numpy python3-pil python3-setuptools
+RUN install_packages python3-numpy python3-pil python3-setuptools python3-influxdb
 RUN pip install enviroplus smbus
 
 WORKDIR /opt/app
--- weather.py
+++ weather.py
@@ -3,29 +3,45 @@
 import time
 from bme280 import BME280
 from smbus import SMBus
-
-import logging
-
-logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
-
-logging.info("""weather.py - Print readings from the BME280 weather sensor.
-
-Press Ctrl+C to exit!
-
-""")
+from influxdb import InfluxDBClient
+from influxdb import SeriesHelper
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+dbClient = InfluxDBClient(host='192.168.86.34', port=8086, username='enviro', password='exXtxtnhlcT5dOOFN4re', database='enviro')
+
+class WeatherHelper(SeriesHelper):
+    """Instantiate SeriesHelper to write points to the backend."""
+
+    class Meta:
+        """Meta class stores time series helper configuration."""
+
+        # The client should be an instance of InfluxDBClient.
+        client = dbClient
+
+        # The series name must be a string. Add dependent fields/tags
+        # in curly brackets.
+        series_name = 'weather.{room}'
+
+        # Defines all the fields in this time series.
+        fields = ['humidity', 'pressure', 'temperature']
+
+        # Defines all the tags for the series.
+        tags = ['room']
+
+        # Defines the number of data points to store prior to writing
+        # on the wire.
+        bulk_size = 5
+
+        # autocommit must be set to True when using bulk_size
+        autocommit = True
+
 while True:
-    temperature = bme280.get_temperature()
-    pressure = bme280.get_pressure()
-    humidity = bme280.get_humidity()
-    logging.info("""Temperature: {:05.2f} *C
-Pressure: {:05.2f} hPa
-Relative humidity: {:05.2f} %
-""".format(temperature, pressure, humidity))
+    temperatureVal = bme280.get_temperature()
+    pressureVal = bme280.get_pressure()
+    humidityVal = bme280.get_humidity()
+
+    WeatherHelper(room='living', humidity=humidityVal, pressure=pressureVal, temperature=temperatureVal)
+
     time.sleep(1)
