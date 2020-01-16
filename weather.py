e3fcbb2453f5d89b22d73f824ab5c647cdbc1d20
Andrea Di Giorgi <adigiorgi@outlook.com>
2020-01-15 21:17:45 -0800

    SF

--- weather.py
+++ weather.py
@@ -9,7 +9,14 @@ from influxdb import SeriesHelper
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
-dbClient = InfluxDBClient(host='192.168.86.34', port=8086, username='enviro', password='exXtxtnhlcT5dOOFN4re', database='enviro')
+dbClient = InfluxDBClient(
+    host="192.168.86.34",
+    port=8086,
+    username="enviro",
+    password="exXtxtnhlcT5dOOFN4re",
+    database="enviro",
+)
+
 
 class WeatherHelper(SeriesHelper):
     """Instantiate SeriesHelper to write points to the backend."""
@@ -22,13 +29,13 @@ class WeatherHelper(SeriesHelper):
 
         # The series name must be a string. Add dependent fields/tags
         # in curly brackets.
-        series_name = 'weather.{room}'
+        series_name = "weather.{room}"
 
         # Defines all the fields in this time series.
-        fields = ['humidity', 'pressure', 'temperature']
+        fields = ["humidity", "pressure", "temperature"]
 
         # Defines all the tags for the series.
-        tags = ['room']
+        tags = ["room"]
 
         # Defines the number of data points to store prior to writing
         # on the wire.
@@ -37,11 +44,17 @@ class WeatherHelper(SeriesHelper):
         # autocommit must be set to True when using bulk_size
         autocommit = True
 
+
 while True:
     temperatureVal = bme280.get_temperature()
     pressureVal = bme280.get_pressure()
     humidityVal = bme280.get_humidity()
 
-    WeatherHelper(room='living', humidity=humidityVal, pressure=pressureVal, temperature=temperatureVal)
+    WeatherHelper(
+        room="living",
+        humidity=humidityVal,
+        pressure=pressureVal,
+        temperature=temperatureVal,
+    )
 
     time.sleep(1)
